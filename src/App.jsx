1862e87fc48983217eecca3976563ad5b654460e
Vedant Nibandhe <vnibandhe@gmail.com>
2021-09-19 14:25:31 -0500

    feature: declared components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './home/Home';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar';
+import './Home.scss'
+const Home = () => {
+    return (
+        <div className = "home">
+            <Navbar />
+        </div>
+    )
+}
+
+export default Home;
